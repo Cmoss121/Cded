0548213a7434ef4a8830f5b6dbba32f4c3f299c8
Cmoss <ddnotti018@gmail.com>
2025-03-28 20:32:46 +0000

    Update Cded.py

--- Cded.py
+++ Cded.py
@@ -2,13 +2,20 @@ from colorama import Fore, Style, init
 
 print(Fore.RED + """
 
- _____     _          _ 
+-----------------------
+
 /  __ \   | |        | |
 | /  \/ __| | ___  __| |
 | |    / _` |/ _ \/ _` |
 | \__/\ (_| |  __/ (_| |
  \____/\__,_|\___|\__,_|
- 
-"""
 
-print("BOMBACLAT")
+-----------------------
+MY DISCORD IS CMOSS23 
+ 
+""")
+
+choice = input("1 For PayloadMaker: ")
+
+if choice == '1':
+	print("Wait...")
