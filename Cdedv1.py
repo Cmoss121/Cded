8fbdddc3f569355551301247e7acc54195cbeeae
Cmoss <ddnotti018@gmail.com>
2025-03-28 20:57:14 +0000

    Update Cdedv1.py

--- Cdedv1.py
+++ Cdedv1.py
@@ -2,15 +2,21 @@ import time
 import sys
 from colorama import Fore, Style, init
 
-def loading_dots():
-    print("Loading", end="")
-    for _ in range(5):
-        time.sleep(0.5)  # Delay
-        print(".", end="", flush=True)
-    print("\nDone!")
+# Initialize colorama to reset colors automatically
+init(autoreset=True)
 
+def loading_dots():
+    """Red loading animation"""
+    print(Fore.RED + "Loading", end="", flush=True)
+    for _ in range(5):
+        time.sleep(0.5)
+        print(".", end="", flush=True)
+    print("\nDone!" + Style.RESET_ALL)
+
+# Run the loading animation
 loading_dots()
 
+# Print ASCII Art in red
 print(Fore.RED + r"""
 
 -----------------------
@@ -24,9 +30,12 @@ print(Fore.RED + r"""
 -----------------------
 MY DISCORD IS CMOSS23 
  
-""")
+""" + Style.RESET_ALL)  # Reset color after ASCII art
 
-choice = input("1 For PayloadMaker: ")
+# Get user choice
+choice = input(Fore.RED + "Enter 1 for PayloadMaker: " + Style.RESET_ALL)
 
 if choice == '1':
-	print("Wait...")
+    print(Fore.RED + "Wait..." + Style.RESET_ALL)
+    time.sleep(2)  # Simulate processing time
+    print(Fore.RED + "PayloadMaker is launching..." + Style.RESET_ALL)
